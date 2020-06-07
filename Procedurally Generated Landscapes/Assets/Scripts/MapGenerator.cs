32ca9e9120a98a57609da60f618c3b427a93b578
tomdotscott <tom.scott45456@gmail.com>
2020-06-07 16:39:06 +0100

    Clamping Values
    To ensure that unity doesn't crash on me again, the values are clamped to their respective ranges

--- Procedurally Generated Landscapes/Assets/Scripts/MapGenerator.cs
+++ Procedurally Generated Landscapes/Assets/Scripts/MapGenerator.cs
@@ -9,6 +9,7 @@ public class MapGenerator : MonoBehaviour
     public int seed;
     public float noiseScale;
     public int octaves;
+    [Range(0, 1)]
     public float persistence;
     public float lacunarity;
     public Vector2 offset;
@@ -21,5 +22,28 @@ public class MapGenerator : MonoBehaviour
 
         MapDisplay mapDisplay = FindObjectOfType<MapDisplay>();
         mapDisplay.DrawNoiseMap(noiseMap);
+    }
+
+    /// <summary>
+    /// Clamps the values of the variables whenever one of them is changed
+    /// </summary>
+    void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
     }
 }
