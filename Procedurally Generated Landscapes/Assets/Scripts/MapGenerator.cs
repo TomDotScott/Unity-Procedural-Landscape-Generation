05485b87a6287cc3561ad14a0e9e3b119f97bb1a
tomdotscott <tom.scott45456@gmail.com>
2020-06-07 16:21:15 +0100

    Persistence, Octaves and Lacunarity
    The complexity of the noise maps generated is now increased now that the map generation accounts for octaves (layers of noise) persistence (how quickly the frequency diminishes per octave) and Lacunarity (how much the frequency increases between octaves).

--- Procedurally Generated Landscapes/Assets/Scripts/MapGenerator.cs
+++ Procedurally Generated Landscapes/Assets/Scripts/MapGenerator.cs
@@ -7,11 +7,16 @@ public class MapGenerator : MonoBehaviour
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
+    public int octaves;
+    public float persistence;
+    public float lacunarity;
+
+
     public bool autoUpdate;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, octaves, persistence, lacunarity);
 
         MapDisplay mapDisplay = FindObjectOfType<MapDisplay>();
         mapDisplay.DrawNoiseMap(noiseMap);
--- Procedurally Generated Landscapes/Assets/Scripts/Noise.cs
+++ Procedurally Generated Landscapes/Assets/Scripts/Noise.cs
@@ -4,27 +4,67 @@ using UnityEngine;
 
 public static class Noise
 {
-    public static float[,] GenerateNoiseMap(int _width, int _height, float _scale)
+    public static float[,] GenerateNoiseMap(int _width, int _height, float _scale, int _octaves, float _persistence, float _lacunarity)
     {
         float[,] noiseMap = new float[_width, _height];
 
-        if(_scale <= 0)
+        if (_scale <= 0)
         {
             _scale = 0.0001f;
         }
 
-        for(int y = 0; y < _height; y++)
-        {
-            for(int x = 0; x < _width; x++)
-            {
-                float sampleX = x / _scale;
-                float sampleY = y / _scale;
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
 
-                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
-                noiseMap[x, y] = perlinValue;
+        for (int y = 0; y < _height; y++)
+        {
+            for (int x = 0; x < _width; x++)
+            {
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+
+                for (int i = 0; i < _octaves; i++)
+                {
+                    // the higher the freuency, the further away the sample points,
+                    // this means the height changes more rapidly
+                    float sampleX = x / _scale * frequency;
+                    float sampleY = y / _scale * frequency;
+
+                    // generate values between -1 and 1 so that there can be dips in the 
+                    // terrain when it is later generated
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= _persistence;
+
+                    // frequency increases per octave since lacunarity is greater 
+                    // than 1.
+                    frequency *= _lacunarity;
+                }
+
+                // work out the range of the noisemap values
+                if (noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }
+                else if (noiseHeight < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+                noiseMap[x, y] = noiseHeight;
             }
         }
+        
 
+        // normalise the noise map
+        for (int y = 0; y < _height; y++)
+        {
+            for (int x = 0; x < _width; x++)
+            {
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+            }
+        } 
         return noiseMap;
     }
 }
