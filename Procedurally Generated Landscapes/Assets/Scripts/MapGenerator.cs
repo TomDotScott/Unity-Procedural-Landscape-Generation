64c5231b98ab9660488db620d635fa2d10e79e38
tomdotscott <tom.scott45456@gmail.com>
2020-06-08 15:54:35 +0100

    Height Values
    Now there is height to the mesh, making the mountains pop.

--- Procedurally Generated Landscapes/Assets/Scripts/MapGenerator.cs
+++ Procedurally Generated Landscapes/Assets/Scripts/MapGenerator.cs
@@ -4,92 +4,92 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    public enum DrawMode
-    {
-        NoiseMap, ColourMap, Mesh
-    };
-    public DrawMode drawMode;
+	public enum DrawMode { NoiseMap, ColourMap, Mesh };
+	public DrawMode drawMode;
 
-    public int mapWidth;
-    public int mapHeight;
-    public int seed;
-    public float noiseScale;
-    public int octaves;
-    [Range(0, 1)]
-    public float persistence;
-    public float lacunarity;
-    public Vector2 offset;
+	public int mapWidth;
+	public int mapHeight;
+	public float noiseScale;
 
-    public bool autoUpdate;
+	public int octaves;
+	[Range(0, 1)]
+	public float persistance;
+	public float lacunarity;
 
-    public TerrainType[] regions;
+	public int seed;
+	public Vector2 offset;
 
-    public void GenerateMap()
-    {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistence, lacunarity, offset);
+	public float heightMultiplier;
 
-        Color[] colourMap = new Color[mapWidth * mapHeight];
+	public bool autoUpdate;
 
-        // Generate the colours for the noise map, depending on their height value
-        for(int y = 0; y < mapHeight; y++)
-        {
-            for (int x = 0; x < mapWidth; x++)
-            {
-                float currentHeight = noiseMap[x, y];
-                for(int i = 0; i < regions.Length; i++)
-                {
-                    if(currentHeight <= regions[i].height)
-                    {
-                        colourMap[y * mapWidth + x] = regions[i].colour;
-                        break;
-                    }
-                }
-            }
+	public TerrainType[] regions;
 
-        }
+	public void GenerateMap()
+	{
+		float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
-        MapDisplay mapDisplay = FindObjectOfType<MapDisplay>();
+		Color[] colourMap = new Color[mapWidth * mapHeight];
+		for (int y = 0; y < mapHeight; y++)
+		{
+			for (int x = 0; x < mapWidth; x++)
+			{
+				float currentHeight = noiseMap[x, y];
+				for (int i = 0; i < regions.Length; i++)
+				{
+					if (currentHeight <= regions[i].height)
+					{
+						colourMap[y * mapWidth + x] = regions[i].colour;
+						break;
+					}
+				}
+			}
+		}
 
-        if (drawMode == DrawMode.NoiseMap)
-        {
-            mapDisplay.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
-        }else if(drawMode == DrawMode.ColourMap)
-        {
-            mapDisplay.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
-        }else if(drawMode == DrawMode.Mesh)
-        {
-            mapDisplay.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
-        }
-    }
+		MapDisplay display = FindObjectOfType<MapDisplay>();
+		if (drawMode == DrawMode.NoiseMap)
+		{
+			display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+		}
+		else if (drawMode == DrawMode.ColourMap)
+		{
+			display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+		}
+		else if (drawMode == DrawMode.Mesh)
+		{
+			display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, heightMultiplier), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+		}
+	}
 
-    /// <summary>
-    /// Clamps the values of the variables whenever one of them is changed
-    /// </summary>
-    void OnValidate()
-    {
-        if (mapWidth < 1)
-        {
-            mapWidth = 1;
-        }
-        if (mapHeight < 1)
-        {
-            mapHeight = 1;
-        }
-        if (lacunarity < 1)
-        {
-            lacunarity = 1;
-        }
-        if (octaves < 0)
-        {
-            octaves = 0;
-        }
-    }
+
+	/// <summary>
+	/// Clamps the values of the variables whenever one of them is changed
+	/// </summary>
+	void OnValidate()
+	{
+		if (mapWidth < 1)
+		{
+			mapWidth = 1;
+		}
+		if (mapHeight < 1)
+		{
+			mapHeight = 1;
+		}
+		if (lacunarity < 1)
+		{
+			lacunarity = 1;
+		}
+		if (octaves < 0)
+		{
+			octaves = 0;
+		}
+	}
 }
 
 [System.Serializable]
 public struct TerrainType
 {
-    public string name;
-    public float height;
-    public Color colour;
+	public string name;
+	public float height;
+	public Color colour;
 }
