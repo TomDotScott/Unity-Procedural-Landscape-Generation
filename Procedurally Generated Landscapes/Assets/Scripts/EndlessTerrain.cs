a3e367d6cb7c1e1f0b685e7912046e21d41048e0
tomdotscott <tom.scott45456@gmail.com>
2020-06-09 14:27:31 +0100

    Endless Terrain
    Planes can now be generated around a given transform. Each chunk has a flag to say whether it is visible or not, depending on its distance to the player object (which is just a cube for now) . There is a dictionary to keep track of all of the chunks visited in the game, and a list to keep tarck of which chunks were visited since the last update loop.

---
+++ Procedurally Generated Landscapes/Assets/Scripts/EndlessTerrain.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessTerrain : MonoBehaviour
+{
+    public const float maxViewDistance = 450;
+    public Transform viewer;
+
+    public static Vector2 viewerPosition;
+
+    int chunkSize;
+    int chunksVisibleInViewDistance;
+
+    Dictionary<Vector2, TerrainChunk> terrainChunkDict = new Dictionary<Vector2, TerrainChunk>();
+    List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
+
+    void Start()
+    {
+        chunkSize = MapGenerator.mapChunkSize - 1;
+        chunksVisibleInViewDistance = Mathf.RoundToInt(maxViewDistance / chunkSize);
+    }
+
+    void Update()
+    {
+        viewerPosition = new Vector2(viewer.position.x, viewer.position.z);
+        UpdateVisibleChunks();
+    }
+
+    void UpdateVisibleChunks()
+    {
+        foreach(var chunk in terrainChunksVisibleLastUpdate)
+        {
+            chunk.SetVisible(false);
+        }
+        terrainChunksVisibleLastUpdate.Clear();
+
+        //find the coordinate of the chunk the player is standing on
+        int currentChunkCoordinateX = Mathf.RoundToInt(viewerPosition.x / chunkSize);
+        int currentChunkCoordinateY = Mathf.RoundToInt(viewerPosition.y / chunkSize);
+
+        for (int yOffset = -chunksVisibleInViewDistance; yOffset <= chunksVisibleInViewDistance; yOffset++)
+        {
+            for (int xOffset = -chunksVisibleInViewDistance; xOffset <= chunksVisibleInViewDistance; xOffset++)
+            {
+                Vector2 viewedChunkCoordinate = new Vector2(currentChunkCoordinateX + xOffset, currentChunkCoordinateY + yOffset);
+                if (terrainChunkDict.ContainsKey(viewedChunkCoordinate))
+                {
+                    //update the terrain chunk
+                    terrainChunkDict[viewedChunkCoordinate].UpdateTerrainChunk();
+                    if (terrainChunkDict[viewedChunkCoordinate].IsVisible())
+                    {
+                        terrainChunksVisibleLastUpdate.Add(terrainChunkDict[viewedChunkCoordinate]);
+                    }
+                }
+                else
+                {
+                    //instantiate new terrain chunk
+                    terrainChunkDict.Add(viewedChunkCoordinate, new TerrainChunk(viewedChunkCoordinate, chunkSize, transform));
+                }
+            }
+        }
+    }
+
+
+    public class TerrainChunk
+    {
+        GameObject meshObject;
+        Vector2 position;
+        Bounds bounds;
+
+        public TerrainChunk(Vector2 _coord, int _size, Transform _parent)
+        {
+            position = _coord * _size;
+            Vector3 positionV3 = new Vector3(position.x, 0, position.y);
+            bounds = new Bounds(position, Vector2.one * _size);
+
+            meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            meshObject.transform.position = positionV3;
+            meshObject.transform.localScale = Vector3.one * _size / 10f;
+            meshObject.transform.parent = _parent;
+            SetVisible(false);
+        }
+
+        public void UpdateTerrainChunk()
+        {
+            float viewerDistance = Mathf.Sqrt(bounds.SqrDistance(viewerPosition));
+            bool visible = viewerDistance <= maxViewDistance;
+            SetVisible(visible);
+        }
+
+        public void SetVisible(bool _visibility)
+        {
+            meshObject.SetActive(_visibility);
+        }
+
+        public bool IsVisible()
+        {
+            return meshObject.activeSelf;
+        }
+    }
+}
--- Procedurally Generated Landscapes/Assets/Scripts/MapGenerator.cs
+++ Procedurally Generated Landscapes/Assets/Scripts/MapGenerator.cs
@@ -7,7 +7,7 @@ public class MapGenerator : MonoBehaviour
 	public enum DrawMode { NoiseMap, ColourMap, Mesh };
 	public DrawMode drawMode;
 
-	const int mapChunkSize = 241;
+	public const int mapChunkSize = 241;
 	[Range(0, 6)]
 	public int levelOfDetail;
 
