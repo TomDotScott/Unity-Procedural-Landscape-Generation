d4bf6b688217cda1b9efe334e6016e225495f3b0
tomdotscott <tom.scott45456@gmail.com>
2020-06-12 13:32:05 +0100

    Terrain Generation Fix
    Terrain can now be infinitely generated again

--- Procedurally Generated Landscapes/Assets/Scripts/MapGenerator.cs
+++ Procedurally Generated Landscapes/Assets/Scripts/MapGenerator.cs
@@ -25,6 +25,11 @@ public class MapGenerator : MonoBehaviour
     Queue<MapThreadInfo<MapData>> mapDataThreadInfoQueue = new Queue<MapThreadInfo<MapData>>();
     Queue<MapThreadInfo<MeshData>> meshDataThreadInfoQueue = new Queue<MapThreadInfo<MeshData>>();
 
+    private void Awake()
+    {
+        textureData.UpdateMeshHeights(terrainMaterial, terrainData.minHeight, terrainData.maxHeight);
+    }
+
     private void OnValuesUpdated()
     {
         if (!Application.isPlaying)
@@ -55,6 +60,8 @@ public class MapGenerator : MonoBehaviour
 
     public void DrawMapInEditor()
     {
+        textureData.UpdateMeshHeights(terrainMaterial, terrainData.minHeight, terrainData.maxHeight);
+
         MapData mapData = GenerateMapData(Vector2.zero);
         MapDisplay display = FindObjectOfType<MapDisplay>();
         if (drawMode == DrawMode.NoiseMap)
@@ -150,9 +157,6 @@ public class MapGenerator : MonoBehaviour
                 }
             }
         }
-
-        textureData.UpdateMeshHeights(terrainMaterial, terrainData.minHeight, terrainData.maxHeight);
-
         return new MapData(noiseMap);
     }
 
